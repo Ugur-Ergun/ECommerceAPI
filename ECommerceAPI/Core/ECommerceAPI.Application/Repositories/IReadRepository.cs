9acf1123b619317e2c7ae43964bad7366bd34e13
Uğur Ergün <ugur.ergun@hotmail.com>
2023-11-18 00:45:47 +0300

    EF Core Tracking Optimization for Get Process

--- ECommerceAPI/Core/ECommerceAPI.Application/Repositories/IReadRepository.cs
+++ ECommerceAPI/Core/ECommerceAPI.Application/Repositories/IReadRepository.cs
@@ -8,11 +8,11 @@ using System.Threading.Tasks;
 
 namespace ECommerceAPI.Application.Repositories
 {
-    public interface IReadRepository<T> :IRepository<T> where T : BaseEntity
+    public interface IReadRepository<T> : IRepository<T> where T : BaseEntity
     {
-        IQueryable<T> GetAll();
-        IQueryable<T> GetWhere(Expression<Func<T,bool>> method);
-        Task<T> GetSingleAsync(Expression<Func<T,bool>> method);
-        Task<T> GetByIdAsync(string id);
+        IQueryable<T> GetAll(bool tracking = false);
+        IQueryable<T> GetWhere(Expression<Func<T, bool>> method, bool tracking = false);
+        Task<T> GetSingleAsync(Expression<Func<T, bool>> method, bool tracking = false);
+        Task<T> GetByIdAsync(string id, bool tracking = false);
     }
 }
