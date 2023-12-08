e422ea03d0cefd87493e6daf52fabe3cea1f39f0
Uğur Ergün <ugur.ergun@hotmail.com>
2023-12-08 23:23:27 +0300

    Add Product Service

--- ECommerceClient/src/app/admin/components/products/products.component.ts
+++ ECommerceClient/src/app/admin/components/products/products.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { BaseComponent, SpinnerType } from 'src/app/base/base.component';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { HttpClientService } from 'src/app/services/common/http-client.service';
-import {Product} from 'src/app/contracts/product';
+import {Create_Product} from 'src/app/contracts/create_product';
 
 @Component({
   selector: 'app-products',
@@ -17,7 +17,7 @@ export class ProductsComponent extends BaseComponent implements OnInit {
 
   ngOnInit(): void {
     this.showSpinner(SpinnerType.BallAtom);
-    this.httpClientService.get<Product[]>({
+    this.httpClientService.get<Create_Product[]>({
       controller: "products"
     }).subscribe(data => console.log(data));
 
